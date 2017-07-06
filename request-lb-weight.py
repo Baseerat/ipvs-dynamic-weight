eb7e4e4ca736a79c123738cde0923697939eccd2
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-07-05 23:13:15 -0500

    Update request-lb-weight.py

--- request-lb-weight.py
+++ request-lb-weight.py
@@ -6,20 +6,19 @@ import os, sys, memcache, time, psutil
 # of the load data is at the same interval (1 minute) then we
 # will see a lot of jerkiness. So we take the 5 minute average.
 # This means servers get lowered in priority more slowly.
-server_id = _SERVER_ID_
 
-if len(sys.argv) < 4:
-    raise Exception('You must pass the hostname and port of your memcached server, '
+if len(sys.argv) < 5:
+    raise Exception('You must pass the host ip address, and hostname and port of your memcached server, '
                     'along with a timeout value for probing the state, debug, metric, and ...')
 
-mc = memcache.Client([sys.argv[1]], )
-
-timeout = float(sys.argv[2])
-debug = sys.argv[3]
-metric = sys.argv[4]
+server_address = sys.argv[1]
+mc = memcache.Client([sys.argv[2]], )
+timeout = float(sys.argv[3])
+debug = sys.argv[4]
+metric = sys.argv[5]
 
 if metric == 'loadavg':
-    max_load = int(sys.argv[5])
+    max_load = int(sys.argv[6])
 
     while True:
         load = os.getloadavg()[1]
@@ -33,13 +32,13 @@ if metric == 'loadavg':
         weight = int(((255 / max_load) * ((max_load + 0.001) - load)) + 1)
 
         if debug == 'True':
-            print 'Declaring weight of %s for %s for %ss' % (weight, server_id, 60 * 2)
+            print 'Declaring weight of %s for %s for %ss' % (weight, server_address, 60 * 2)
 
-        mc.set('server-weight-%s' % server_id, weight, time=60 * 2)  # Set a two minute expiry
+        mc.set('%s-weight' % server_address, weight, time=60 * 2)  # Set a two minute expiry
         time.sleep(timeout)
 
 elif metric == 'cpu':
-    alpha_value = float(sys.argv[5])
+    alpha_value = float(sys.argv[6])
     inv_alpha_value = 1 - alpha_value
 
 
@@ -66,9 +65,9 @@ elif metric == 'cpu':
         # else: weight = XXX
 
         if debug == 'True':
-            print 'Declaring weight of %s for %s for %ss' % (weight, server_id, 60 * 2)
+            print 'Declaring weight of %s for %s for %ss' % (weight, server_address, 60 * 2)
 
         last_idle, last_total = idle, total
 
-        mc.set('server-weight-%s' % server_id, weight, time=60 * 2)  # Set a two minute expiry
+        mc.set('%s-weight' % server_address, weight, time=60 * 2)  # Set a two minute expiry
         time.sleep(timeout)
